366452e18e705c203c4bd3b823e5bc336648ed5d
prodaigo <soccer.0125.dtp@gmail.com>
2022-05-10 16:00:41 +0900

    5/9の進捗

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Blog;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            BlogsTableSeeder::class,
+        ]);
     }
 }
